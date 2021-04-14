6d4ace46882cf612351d474aa8e517593c86dcf4
Andrew Carbert <ac.believer@gmail.com>
2021-04-13 23:06:55 -0600

    merged auto start and auto start into a single manager script

--- Event Manager.py
+++ Event Manager.py
@@ -2,11 +2,11 @@ import obspython as obs
 import math
 from datetime import time, timedelta, datetime
 
-version = "2.0"
+version = "2.1"
 
 days_of_week = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ]
 
-weekday = -1
+weekday = 0
 livestream_start = 37500 # 10:25 AM
 
 start_scene = ""
@@ -15,21 +15,44 @@ preshow_triggered = False
 preshow_duration = 1*60*5
 
 text_source = ""
-countdown_duration = 60
-countdown_offset = -10
 countdown_final_text = "Live Soon!"
 
 auto_start_streaming = True
 auto_start_recording = True
+
+closing_scene = ""
+auto_stop_streaming = True
+stop_streaming_delay = 60
+auto_stop_recording = True
+stop_recording_delay = 0
 
 def event_callback_start_recording(event):
 	if event == obs.OBS_FRONTEND_EVENT_SCENE_CHANGED:
 		if auto_start_recording and not obs.obs_frontend_recording_active():
 			obs.obs_frontend_recording_start()
 
+def event_callback_stop_event(event):
+	if event == obs.OBS_FRONTEND_EVENT_SCENE_CHANGED:
+		if current_scene_name() == closing_scene:
+			if auto_recording and obs.obs_frontend_recording_active():
+				if recording_delay == 0:
+					stop_recording()
+				else:
+					obs.timer_add(stop_recording, recording_delay * 1000)
+			if auto_streaming and obs.obs_frontend_streaming_active():
+				if streaming_delay == 0:
+					stop_streaming()
+				else:
+					obs.timer_add(stop_streaming, streaming_delay * 1000)
+		else:
+			if auto_recording and obs.obs_frontend_recording_active():
+				obs.timer_remove(stop_recording)
+			if auto_streaming and obs.obs_frontend_streaming_active():
+				obs.timer_remove(stop_streaming)
+
 def update_countdown():
 	text = ""
-	t = diff_time().total_seconds() - countdown_offset
+	t = diff_time().total_seconds()
 	if t < 0:
 		text = countdown_final_text
 		obs.remove_current_callback()
@@ -84,6 +107,16 @@ def check_start():
 		obs.timer_add(update_countdown, 1000)
 		obs.remove_current_callback()
 
+def stop_streaming():
+	if auto_streaming and obs.obs_frontend_streaming_active() and current_scene_name() == closing_scene:
+		obs.obs_frontend_streaming_stop()
+	obs.timer_remove(stop_streaming)
+
+def stop_recording():
+	if auto_recording and obs.obs_frontend_recording_active() and current_scene_name() == closing_scene:
+		obs.obs_frontend_recording_stop()
+	obs.timer_remove(stop_recording)
+
 def script_description():
 	return "Automatically starts a scheduled event.\nv" + version
 
@@ -95,54 +128,69 @@ def livestream_start_list_modified(props, property, settings):
 		obs.obs_property_list_add_int(event_start_list, time(hour=math.floor((livestream_start+seconds)/60/60), minute=int((livestream_start+seconds)/60%60)).strftime("%I:%M %p"), seconds)
 	return True
 
+def closing_scene_modified(props, properties, settings):
+	obs.obs_property_set_enabled(obs.obs_properties_get(props, "auto_stop_streaming"), closing_scene != "")
+	obs.obs_property_set_enabled(obs.obs_properties_get(props, "stop_streaming_delay"), closing_scene != "" and auto_stop_streaming)
+	obs.obs_property_set_enabled(obs.obs_properties_get(props, "auto_stop_recording"), closing_scene != "")
+	obs.obs_property_set_enabled(obs.obs_properties_get(props, "stop_recording_delay"), closing_scene != "" and auto_stop_recording)
+	return True
+
+def auto_stop_streaming_modified(props, properties, settings):
+	obs.obs_property_set_enabled(obs.obs_properties_get(props, "stop_streaming_delay"), closing_scene != "" and auto_stop_streaming)
+	return True
+
+def auto_stop_recording_modified(props, properties, settings):
+	obs.obs_property_set_enabled(obs.obs_properties_get(props, "stop_recording_delay"), closing_scene != "" and auto_stop_recording)
+	return True
+
 def on_property_modified(props, property, settings):
-	obs.obs_property_set_enabled(obs.obs_properties_get(props, "livestream_start"), weekday > -1)
-	obs.obs_property_set_enabled(obs.obs_properties_get(props, "preshow_duration"), weekday > -1)
-	obs.obs_property_set_enabled(obs.obs_properties_get(props, "start_scene"), weekday > -1)
-	obs.obs_property_set_enabled(obs.obs_properties_get(props, "auto_start_streaming"), weekday > -1)
-	obs.obs_property_set_enabled(obs.obs_properties_get(props, "auto_start_recording"), weekday > -1)
-	obs.obs_property_set_enabled(obs.obs_properties_get(props, "text_source"), weekday > -1)
-	obs.obs_property_set_enabled(obs.obs_properties_get(props, "countdown_offset"), weekday > -1 and text_source != "")
-	obs.obs_property_set_enabled(obs.obs_properties_get(props, "countdown_final_text"), weekday > -1 and text_source != "")
+	obs.obs_property_set_enabled(obs.obs_properties_get(props, "start_scene"), auto_start_streaming or auto_start_recording)
+	obs.obs_property_set_enabled(obs.obs_properties_get(props, "weekday"), auto_start_streaming or auto_start_recording)
+	obs.obs_property_set_enabled(obs.obs_properties_get(props, "livestream_start"), auto_start_streaming or auto_start_recording)
+	obs.obs_property_set_enabled(obs.obs_properties_get(props, "preshow_duration"), auto_start_streaming or auto_start_recording)
+	obs.obs_property_set_enabled(obs.obs_properties_get(props, "text_source"), auto_start_streaming or auto_start_recording)
+	obs.obs_property_set_enabled(obs.obs_properties_get(props, "countdown_final_text"), (auto_start_streaming or auto_start_recording) and text_source != "")
 	return True
 
 def script_properties():
 	props = obs.obs_properties_create()
-	
-	schedule_group = obs.obs_properties_create()
-	p = obs.obs_properties_add_list(schedule_group, "weekday", "Day of Week", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_INT)
+
+	auto_start_group = obs.obs_properties_create()
+	obs.obs_properties_add_group(props, "auto_start_group", "Auto Start", obs.OBS_GROUP_NORMAL, auto_start_group)
+
+	p = obs.obs_properties_add_bool(auto_start_group, "auto_start_streaming", "Auto Start Streaming")
 	obs.obs_property_set_modified_callback(p, on_property_modified)
-	obs.obs_property_list_add_int(p, "--Disabled--", -1)
+	p = obs.obs_properties_add_bool(auto_start_group, "auto_start_recording", "Auto Start Recording")
+	obs.obs_property_set_modified_callback(p, on_property_modified)
+
+	p = obs.obs_properties_add_list(auto_start_group, "start_scene", "Start Scene", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+	obs.obs_property_set_enabled(p, auto_start_streaming or auto_start_recording)
+	scene_names = obs.obs_frontend_get_scene_names()
+	if scene_names != None:
+		for scene_name in scene_names:
+			obs.obs_property_list_add_string(p, scene_name, scene_name)
+
+	schedule_group = obs.obs_properties_create()
+	obs.obs_properties_add_group(auto_start_group, "schedule_group", "Schedule", obs.OBS_GROUP_NORMAL, schedule_group)
+
+	p = obs.obs_properties_add_list(schedule_group, "weekday", "Day of Week", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_INT)
 	for i, day_of_week in enumerate(days_of_week):
 		obs.obs_property_list_add_int(p, day_of_week, i)
 	p = obs.obs_properties_add_list(schedule_group, "livestream_start", "Livestream Start", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_INT)
-	obs.obs_property_set_enabled(p, weekday > -1)
+	obs.obs_property_set_enabled(p, auto_start_streaming or auto_start_recording)
 	obs.obs_property_set_modified_callback(p, livestream_start_list_modified)
 	for i in range(288):
 		obs.obs_property_list_add_int(p, time(hour=math.floor(i/12), minute=(i%12)*5).strftime("%I:%M %p"), int((i/12)*60*60))
 		
 	p = obs.obs_properties_add_list(schedule_group, "preshow_duration", "Event Start", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_INT)
-	obs.obs_property_set_enabled(p, weekday > -1)
+	obs.obs_property_set_enabled(p, auto_start_streaming or auto_start_recording)
 	obs.obs_property_list_clear(p)
 	for i in range(4):
 		seconds = i*60*5
 		obs.obs_property_list_add_int(p, time(hour=math.floor((livestream_start+seconds)/60/60), minute=int((livestream_start+seconds)/60%60)).strftime("%I:%M %p"), seconds)
-	obs.obs_properties_add_group(props, "schedule_group", "Schedule", obs.OBS_GROUP_NORMAL, schedule_group)
 
-	p = obs.obs_properties_add_list(props, "start_scene", "Start Scene", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
-	obs.obs_property_set_enabled(p, weekday > -1)
-	scene_names = obs.obs_frontend_get_scene_names()
-	if scene_names != None:
-		for scene_name in scene_names:
-			obs.obs_property_list_add_string(p, scene_name, scene_name)
-	
-	p = obs.obs_properties_add_bool(props, "auto_start_streaming", "Auto Start Streaming")
-	obs.obs_property_set_enabled(p, weekday > -1)
-	p = obs.obs_properties_add_bool(props, "auto_start_recording", "Auto Start Recording")
-	obs.obs_property_set_enabled(p, weekday > -1)
-
-	text_source_list = obs.obs_properties_add_list(props, "text_source", "Text Source", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
-	obs.obs_property_set_enabled(text_source_list, weekday > -1)
+	text_source_list = obs.obs_properties_add_list(auto_start_group, "text_source", "Text Source", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+	obs.obs_property_set_enabled(text_source_list, auto_start_streaming or auto_start_recording)
 	obs.obs_property_set_modified_callback(text_source_list, on_property_modified)
 	obs.obs_property_list_add_string(text_source_list, "--Disabled--", "")	
 
@@ -155,10 +203,28 @@ def script_properties():
 				obs.obs_property_list_add_string(text_source_list, name, name)
 	obs.source_list_release(sources)
 
-	p = obs.obs_properties_add_int_slider(props, "countdown_offset", "Countdown Offset", -300, 300, 10)
-	obs.obs_property_set_enabled(p, weekday > -1 and text_source != "")
-	p = obs.obs_properties_add_text(props, "countdown_final_text", "Countdown Final Text", obs.OBS_TEXT_DEFAULT)
-	obs.obs_property_set_enabled(p, weekday > -1 and text_source != "")
+	p = obs.obs_properties_add_text(auto_start_group, "countdown_final_text", "Countdown Final Text", obs.OBS_TEXT_DEFAULT)
+	obs.obs_property_set_enabled(p, (auto_start_streaming or auto_start_recording) and text_source != "")
+
+	auto_stop_group = obs.obs_properties_create()
+	obs.obs_properties_add_group(props, "auto_stop_group", "Auto Stop", obs.OBS_GROUP_NORMAL, auto_stop_group)
+
+	p = obs.obs_properties_add_list(auto_stop_group, "closing_scene", "Closing Scene", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+	obs.obs_property_set_modified_callback(p, closing_scene_modified)
+	obs.obs_property_list_add_string(p, "--Disabled--", "")
+	scene_names = obs.obs_frontend_get_scene_names()
+	if scene_names != None:
+		for scene_name in scene_names:
+			obs.obs_property_list_add_string(p, scene_name, scene_name)
+	
+
+	p = obs.obs_properties_add_bool(auto_stop_group, "auto_stop_streaming", "Auto Stop Streaming")
+	obs.obs_property_set_modified_callback(p, auto_stop_streaming_modified)
+	obs.obs_properties_add_int(auto_stop_group, "stop_streaming_delay", "Streaming Delay", 0, 300, 15)
+
+	p = obs.obs_properties_add_bool(auto_stop_group, "auto_stop_recording", "Auto Stop Recording")
+	obs.obs_property_set_modified_callback(p, auto_stop_recording_modified)
+	obs.obs_properties_add_int(auto_stop_group, "stop_recording_delay", "Recording Delay", 0, 120, 5)
 
 	return props
 
@@ -168,11 +234,15 @@ def script_defaults(settings):
 
 	obs.obs_data_set_default_int(settings, "preshow_duration", preshow_duration)
 
-	obs.obs_data_set_default_int(settings, "countdown_offset", countdown_offset)
 	obs.obs_data_set_default_string(settings, "countdown_final_text", countdown_final_text)
 
 	obs.obs_data_set_default_bool(settings, "auto_start_streaming", auto_start_streaming)
-	obs.obs_data_set_default_bool(settings, "auto_start_recording", auto_start_recording)
+	obs.obs_data_set_default_bool(settings, "auto_start_recording", auto_start_recording)	
+
+	obs.obs_data_set_default_bool(settings, "auto_stop_streaming", auto_stop_streaming)
+	obs.obs_data_set_default_int(settings, "stop_streaming_delay", stop_streaming_delay)
+	obs.obs_data_set_default_bool(settings, "auto_stop_recording", auto_stop_recording)
+	obs.obs_data_set_default_int(settings, "stop_recording_delay", stop_recording_delay)
 
 def script_update(settings):
 	global weekday
@@ -196,13 +266,28 @@ def script_update(settings):
 
 	global text_source
 	text_source = obs.obs_data_get_string(settings, "text_source")
-	global countdown_offset
-	countdown_offset = obs.obs_data_get_int(settings, "countdown_offset") * -1
 	global countdown_final_text
 	countdown_final_text = obs.obs_data_get_string(settings, "countdown_final_text")
 
+	global auto_stop_streaming
+	auto_stop_streaming = obs.obs_data_get_bool(settings, "auto_stop_streaming")
+	global stop_streaming_delay
+	stop_streaming_delay = obs.obs_data_get_int(settings, "stop_streaming_delay")
+	global auto_stop_recording
+	auto_stop_recording = obs.obs_data_get_bool(settings, "auto_stop_recording")
+	global stop_recording_delay
+	stop_recording_delay = obs.obs_data_get_int(settings, "stop_recording_delay")
+	global closing_scene
+	closing_scene = obs.obs_data_get_string(settings, "closing_scene")
+
+def script_load(settings):
+	print("script load")
 	obs.timer_remove(check_start)
 	obs.timer_remove(update_countdown)
 	set_text_source("")
-	if weekday == datetime.now().weekday() and diff_time().total_seconds() > countdown_offset and not (obs.obs_frontend_streaming_active() or obs.obs_frontend_recording_active()):
+	obs.obs_frontend_remove_event_callback(event_callback_start_recording)
+	if (auto_start_streaming or auto_start_recording) and weekday == datetime.now().weekday() and diff_time().total_seconds() > 0 and not (obs.obs_frontend_streaming_active() or obs.obs_frontend_recording_active()):
 		obs.timer_add(check_start, 1000)
+
+	obs.obs_frontend_remove_event_callback(event_callback_stop_event)
+	obs.obs_frontend_add_event_callback(event_callback_stop_event)
